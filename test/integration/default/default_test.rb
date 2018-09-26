dba8dbb37fe6e306ddea2d87b78697c0a3ac5cdc
Hussin Saheb <Hsaheb@spartaglobal.com>
2018-09-26 16:48:20 +0100

    added integration tests for python and its dependencies

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -8,3 +8,13 @@
 
 
 # This is an example test, replace it with your own test.
+describe package("python") do
+  it { should be_installed }
+  its("version") { should match /2.[0-9.]*/ }
+end
+describe package("python-pip") do
+  it { should be_installed }
+end
+describe package("libncurses5-dev") do
+  it { should be_installed }
+end
